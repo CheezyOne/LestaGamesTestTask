1c9a519c01ef6c1fdf274be52d8008a7103f9a10
CheezyOne <Lavrovvova0103@gmail.com>
2025-09-02 09:59:26 +0300

    Adjusted game according to the new rules

--- Assets/Scripts/Gameplay/LevelUpWindow.cs
+++ Assets/Scripts/Gameplay/LevelUpWindow.cs
@@ -6,12 +6,44 @@ public class LevelUpWindow : BaseWindow
     [SerializeField] private TMP_Text _currentBarbarianLevel;
     [SerializeField] private TMP_Text _currentAssasinLevel;
     [SerializeField] private TMP_Text _currentWarriorLevel;
+    [SerializeField] private GameObject _barbarianLevelUpButton;
+    [SerializeField] private GameObject _assasinLevelUpButton;
+    [SerializeField] private GameObject _warriorLevelUpButton;
+    [SerializeField] private int _maxLevel;
+    [SerializeField] private string _maxLevelText;
 
     public override void Init()
     {
-        _currentBarbarianLevel.text = PlayerSpawner.Instance.CharacterLevels.BarbarianLevel.ToString();
-        _currentAssasinLevel.text = PlayerSpawner.Instance.CharacterLevels.AssasinLevel.ToString();
-        _currentWarriorLevel.text = PlayerSpawner.Instance.CharacterLevels.WarriorLevel.ToString();
+        if(PlayerSpawner.Instance.CharacterLevels.BarbarianLevel == _maxLevel)
+        {
+            _currentBarbarianLevel.text = _maxLevelText;
+            _barbarianLevelUpButton.SetActive(false);
+        }
+        else
+        {
+            _currentBarbarianLevel.text = PlayerSpawner.Instance.CharacterLevels.BarbarianLevel.ToString();
+        }
+
+        if (PlayerSpawner.Instance.CharacterLevels.AssasinLevel == _maxLevel)
+        {
+            _currentAssasinLevel.text = _maxLevelText;
+            _assasinLevelUpButton.SetActive(false);
+        }
+        else
+        {
+            _currentAssasinLevel.text = PlayerSpawner.Instance.CharacterLevels.AssasinLevel.ToString();
+        }
+
+        if (PlayerSpawner.Instance.CharacterLevels.WarriorLevel == _maxLevel)
+        {
+            _currentWarriorLevel.text = _maxLevelText;
+            _warriorLevelUpButton.SetActive(false);
+        }
+        else
+        {
+            _currentWarriorLevel.text = PlayerSpawner.Instance.CharacterLevels.WarriorLevel.ToString();
+        }
+
         EventBus.OnLevelUpWindowOpened?.Invoke();
     }
 
